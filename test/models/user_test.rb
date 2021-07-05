ec239ed18615801c07911076727ee21cce60c78f
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-05 14:39:45 +0200

    add basic validation to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # def setup
+  #   @user = User.new(username: "User", fullname: "example user",email: "user@example.com")
+  # end
+
+  # test "should be valid" do
+  #   assert @user.valid?
+  # end
+
+  # test "username should be present" do
+  #    @user.username = ""
+  #    assert_not @user.valid?
+  # end
+
+  #  test "username should be present" do
+  #    @user.email = ""
+  #    assert_not @user.valid?
   # end
 end
