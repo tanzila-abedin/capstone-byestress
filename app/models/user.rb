1b04adda9b4264a02da5946667aaf8e0947536b0
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-06 11:09:43 +0200

    Add basic user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true,
-  validates :fullname, presence: true,
-  validates :email, presence: true,
+  validates :username, presence: true, length: {maximum: 50}
+  validates :fullname, presence: true, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                         format: { with: VALID_EMAIL_REGEX },
+                                            uniqueness: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Auction, :type => :model do
+
+
+  
+  it "is not valid without a username" do
+    username = User.new(username: nil)
+    expect(username).to_not be_valid
+  end
+
+   it "is not valid without a full name" do
+    fullname = User.new(fullname: nil)
+    expect(fullname).to_not be_valid
+  end
+
+   it "is not valid without a full name" do
+    email = User.new(email: nil)
+    expect(email).to_not be_valid
+  end
+end
