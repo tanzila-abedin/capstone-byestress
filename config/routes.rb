1c084e01a3c9c6fe2ef37782962b46d9b7ae654b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-07 13:35:32 +0200

    Add user resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'microposts/new'
-  get 'followings/new'
-  get 'users/new'
+  root "microposts#index"
   devise_for :users
+  resources :users, only: [:index, :show]
+
+
+
+  # get 'microposts/new'
+  # get 'followings/new'
+  # get 'users/new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root "application#index"
+  #  root "application#index"
 end
