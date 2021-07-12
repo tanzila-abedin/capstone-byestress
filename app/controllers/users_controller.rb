1abc9c99b24c84629c002d298aca6811ad1cfcb7
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-12 11:33:20 +0200

    Add following and followers action to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,53 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
+  before_action :authenticate_user!
+  
+  def index
+  @users = User.all
+  end
+
   def new
   end
+  
   def show
-  
+    @user = User.find_by(id: params[:id])
   end
-  def index
-  
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+     end
+  end
+ 
+  def logged_in?
+    !current_user.nil?
+  end
+
+
+  def me?(user)
+    current_user == user
+  end
+
+
+  def user_params
+    params.require(:user).permit(:username, :fullname, :email)
   end
 end
