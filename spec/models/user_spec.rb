3be55dbf828f90d8be5d4e4b049f988b9ef956f3
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-16 16:11:33 +0200

    Add User model and validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe User, :type => :model do
     email = User.new(email: nil)
     expect(email).to_not be_valid
   end
+
+
+it 'has many likes' do
+ like = User.reflect_on_association(:likes)
+ expect(like.macro).to eq(:has_many)
 end
+
+it 'has many micropost' do
+ micropost = User.reflect_on_association(:microposts)
+ expect(micropost.macro).to eq(:has_many)
+end
+
+it 'has many following' do
+ following = User.reflect_on_association(:following)
+ expect(following.macro).to eq(:has_many)
+end
+
+it 'has many followers' do
+ followers = User.reflect_on_association(:followers)
+ expect(followers.macro).to eq(:has_many)
+end
+
+end
+
