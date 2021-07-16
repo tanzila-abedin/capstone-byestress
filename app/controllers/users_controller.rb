9a3d00c6d0ca8ffdb67e8a7d8c1680054a820d86
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-17 00:53:18 +0200

    Fix error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,9 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
     end
   end
 
