863dba0c38c5b4ed17c24d8cbcd5aa3aa7bb4bbb
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-16 15:49:43 +0200

    Add create   and destroy function like controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+  def create
+    micropost = Micropost.find_by(id: params[:format].to_i)
+    current_user.liked_posts << micropost
+    redirect_to root_path
+  end
+
+  def destroy
+  like = Like.find_by(user_id: current_user.id, micropost_id: params[:id])
+  like.delete
+  redirect_to root_path
+  end
+end
