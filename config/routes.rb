db385c446c80922877cbdee80d7661591199c7ee
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-16 15:47:25 +0200

    Add like routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   root "microposts#index"
   devise_for :users
-  resources :users do
-    member do 
-     get :following, :followers
-    end
-  end
+  resources :users 
   resources :microposts, only: [:index, :create, :new, :destroy]
+
+  resources :likes, only: [:create, :destroy]
+
+
   resources :followings, only: [:create, :destroy]
-
-
-  # get 'microposts/new'
-  # get 'followings/new'
-  # get 'users/new'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #  root "application#index"
 end
